a187fb284322f6f1bec5094de3e68b15cf6e2d8f
Administrator <zhongruigo@gmail.com>
2024-05-08 10:46:03 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int len=9;
+    int half=len/2;
+    for (int i=0;i<len/2;i++){
+        for (int j=0;j<half-i;j++){
+            printf(" ");
+        }
+
+        for (int j=0;j<2*i+1;j++){
+            printf("*");
+        }
+
+        printf("\n");
+    }
+    for (int j=0;j<2*half+1;j++){
+        printf("*");
+    }
+    printf("\n");
+    for (int i=len/2;i>0;i--){
+        for (int j=half-i+1;j>0;j--){
+            printf(" ");
+        }
+
+        for (int j=2*i-1;j>0;j--){
+            printf("*");
+        }
+
+        printf("\n");
+    }
+}
